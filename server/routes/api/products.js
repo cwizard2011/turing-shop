a347cec3bdfb1e513bcd2d8c1a02523f8a8b9458
Adeoye Peter Adeola <cwizard2011@gmail.com>
2019-06-22 21:01:17 +0100

    Update products.js

--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import ProductController from '../../controller/ProductController';
 import QueryValidation from '../../middlewares/validations/QueryValidation';
-import redisMiddleware from '../../helpers/cache';
 
 const router = express.Router();
 
 // Items endpoints
 router.get('/items',
   QueryValidation.queryValidation,
-  redisMiddleware,
   ProductController.getAllItems);
 router.get('/items/:id', ProductController.getSingleItem);
 router.get('/department', ProductController.getAllDepartments);
